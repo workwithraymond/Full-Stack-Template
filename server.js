04843575a6c7fb0dc4bcba85cf86391c0f4f63f2
workwithraymond <workwithraymond01@gmail.com>
2023-05-31 06:46:14 -0400

    adding middleware

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+//middleware to add ejs. This generates html
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true})) //this parses urls
+app.use(express.json())//this middleware parses the json, it helps us read the data that is comming back and forth
+app.use(cors())
 
     
 app.listen(process.env.PORT || PORT, () => {
