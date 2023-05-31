494240fe6706dd0dfea1833fdf1ffa763ec551f4
workwithraymond <workwithraymond01@gmail.com>
2023-05-30 20:45:57 -0400

    work now

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+//step two (Require dependencies)
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8200
 
+
+//Declare variables 
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'sample_mflix',
     collection
 
+//Connect to MongoDB 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log(`Connected to Database`)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+    
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
 
